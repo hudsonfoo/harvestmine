f1b402c335dc6e20ff9cbfd0c9617a1448800b84
David Hudson <im@davidhudson.me>
2012-11-07 09:38:13 -0600

    Major updates to service requester for redmine v1 support.

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,20 +12,19 @@ require([
 	"dojo/data/ItemFileWriteStore",
 	"dojo/on"
 ], function(query, dom, domAttr, domClass, domConstruct, atomModel, tooltip, DataGrid, parser, Memory, ItemFileWriteStore, on) {
-	// Get activity list
-	//http://redmine.davidandgoliath.com/projects/high4204/issues.atom?key=2173264169ef659e358ba91b028f9f18fccb5235
-	
 	// Get list of projects
 	dojo.xhrGet({
 		url: "service/fetch.php",
 		handleAs: 'xml',
-		content: { url: redmineUrl + "/projects.xml?key=" + apiKey, "contentType" : "atom" },
+		content: {
+            'serviceName': 'getProjects'
+        },
 		load: function(xmlDoc) {
 			var projects = xmlDoc.getElementsByTagName("project");
 			var projectsStore = new Memory();
 
-			// Create data store
-			var columns = Array('name','identifier','created_on','updated_on');			
+            var columns = Array("id", "name", "created_on", "updated_on" )
+			// Create projects data store
 			for (var i in projects) {
 				if (typeof projects[i] == "object") {
 					var thisProject = projects[i];
@@ -36,65 +35,83 @@ require([
 					}
 				}
 			}
-			
-			// Build Data Grid
-		    var layout = [[
-				{'name': 'ID', 'field': 'id', 'width': '20%'},
-				{'name': 'Name', 'field': 'name', 'width': '20%'},
-				{'name': 'Identifier', 'field': 'identifier', 'width': '20%'},
-				{'name': 'Created On', 'field': 'created_on', 'width': '20%'},
-				{'name': 'Updated On', 'field': 'updated_on', 'width': '20%'}
-			]];
 
-			var projectsListGrid = new dojox.grid.DataGrid({
-				id: 'projectsListGrid',
-				store: new ItemFileWriteStore({ data: { identifier : 'id', items : projectsStore.data } }),
-				structure: layout,
-				autoHeight: true
-			}, document.createElement('div'));
-			
-			dojo.byId("projectsList").appendChild(projectsListGrid.domNode);
-			projectsListGrid.startup();
+            // Get list of issues
+            dojo.xhrGet({
+                url: "service/fetch.php",
+                handleAs: "json",
+                content: {
+                    'serviceName': 'getIssues',
+                    'project_id': thisProjectId
+                },
+                load: function(issues) {
+                    // Link data from other stores to this issue
+                    for (var i in issues) {
+                        issues[i]['project_name'] = (projectsStore.get(issues[i]['project_id'])).name;
+                        issues[i]['timer_running'] = false;
+                    }
+
+                    var issuesStore = new Memory({ data : issues, idProperty : "id" });
+
+                    // Build Data Grid
+                    var layout = [{
+                        onAfterRow : function(rowIndex, cells, rowNode) {
+                            var thisTooltip = issuesListGrid.getItem(rowIndex) && issuesListGrid.store.getValue(issuesListGrid.getItem(rowIndex), "description") != "" ? new tooltip({ connectId: rowNode, label: issuesListGrid.store.getValue(issuesListGrid.getItem(rowIndex), "description"), position:["above", "below"] }) : false;
+                        },
+                        cells : [
+                            {'name': 'Project ID', 'field': 'project_id', 'hidden': true},
+                            {'name': 'Project Name', 'field': 'project_name', 'width': '25%'},
+                            {'name': 'Issue ID', 'field': 'id', 'width': '5%'},
+                            {'name': 'Subject', 'field': 'subject', 'width': '45%', editable : true},
+                            {'name': 'Status', 'field': 'status_id', 'width': '5%', 'styles': 'text-align: center;', editable: true, type: dojox.grid.cells.Select, options: [1,2,3,4] }
+                        ]
+                    }];
+
+                    var issuesListGrid = new dojox.grid.DataGrid({
+                        id: 'issuesListGrid',
+                        store: new ItemFileWriteStore({ data: { identifier : 'id', items : issuesStore.data } }),
+                        structure: layout,
+                        autoHeight: true,
+                        selectionMode: 'single',
+                        singleClickEdit: true,
+                        onRowDblClick: function(a,b,c) {
+                            console.log(a,b,c);
+                        }
+                    }, document.createElement('div'));
+
+                    dojo.byId("issuesList").appendChild(issuesListGrid.domNode);
+                    issuesListGrid.startup();
+
+                    /* Grid Events */
+                    // Edit Issue Subject or Status ID
+                    on (issuesListGrid, 'applyCellEdit', function(inValue, rowIndex, inFieldIndex) {
+                        // If this is the subject or the status, push it to service
+                        if (inFieldIndex == "subject" || inFieldIndex == "status_id") {
+                            var thisIssueId = issuesListGrid.store.getValue(issuesListGrid.getItem(rowIndex), "id");
+
+                            var putObject = {};
+                            putObject.issue = {};
+                            putObject.issue[inFieldIndex] = inValue;
+
+                            dojo.xhrGet({
+                                url: "service/fetch.php",
+                                content: {
+                                    'serviceName': 'putIssue',
+                                    'data': dojo.toJson(putObject)
+                                },
+                                load: function(result) {
+                                    console.log(result);
+                                }
+                            });
+                        }
+                    });
+                    // Clock In/Out
+                }
+            });
 		},
 		error: function(error){
 			console.log(error);
 		}
 	});
-	
-	// Get list of issues
-	dojo.xhrGet({
-		url: "service/fetch.php",
-		handleAs: "json",
-		content: { url: redmineUrl + "/issues.json?key=" + apiKey, "contentType" : "json" },
-		load: function(issues) {
-			//thisTooltip = issues[i].description != "" ? new tooltip({ connectId : thisRow, label : issues[i].description, position : ["above", "below"] }) : false;
-			var issuesStore = new Memory({ data : issues, idProperty : "id" });
 
-			// Build Data Grid
-		    var layout = [{
-				onAfterRow : function(rowIndex, cells, rowNode) {
-					var thisTooltip = issuesListGrid.getItem(rowIndex) && issuesListGrid.store.getValue(issuesListGrid.getItem(rowIndex), "description") != "" ? new tooltip({ connectId: rowNode, label: issuesListGrid.store.getValue(issuesListGrid.getItem(rowIndex), "description") }) : false;
-				},
-				cells : [
-					{'name': 'ID', 'field': 'id', 'width': '5%'},
-					{'name': 'Subject', 'field': 'subject', 'width': '75%', editable : true},
-					{'name': 'Status', 'field': 'status_id', 'width': '10%', 'styles': 'text-align: center;' },
-					{'name': 'Total Time', 'field': 'totalTime', 'width': '10%'}
-				]
-			}];
-
-			var issuesListGrid = new dojox.grid.DataGrid({
-				id: 'issuesListGrid',
-				store: new ItemFileWriteStore({ data: { identifier : 'id', items : issuesStore.data } }),
-				structure: layout,
-				autoHeight: true
-			}, document.createElement('div'));
-			
-			dojo.byId("projectsList").appendChild(issuesListGrid.domNode);
-			issuesListGrid.startup();
-			on (issuesListGrid, 'applyCellEdit', function(inValue, rowIndex, inFieldIndex) {
-				console.log(inValue, rowIndex, inFieldIndex);
-			});
-		}
-	});
 });
--- service/fetch.php
+++ service/fetch.php
@@ -1,70 +1,168 @@
 <?php
+$harvestServerUrl = 'https://idealoop.harvestapp.com/account/who_am_i';
+$redmineServerUrl = 'http://redmine.davidandgoliath.com';
+$redmineApiKey = '0ef9a2dcceccc18e1aa6a0f5b1e4c7f1ae8e2429';
+$harvestUserId = 'your@id.com';
+$harvestPassword = 'yourP4ssw0rd';
 
-	$headers = array(
-		'Accept: application/json',
-		$contentType,
-	);
+$service['getNews']['url'] = '/projects/%project_id%/news.xml';
+$service['getNews']['callType'] = 'GET';
+$service['getNews']['requestContentType'] = 'xml';
+$service['getNews']['responseContentType'] = 'atom';
 
-	$handle = curl_init();
-	curl_setopt($handle, CURLOPT_URL, $_GET['url']);
-	curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
-	curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, false);
+$service['getProjects']['url'] = '/projects.xml';
+$service['getProjects']['callType'] = 'GET';
+$service['getProjects']['requestContentType'] = 'xml';
+$service['getProjects']['responseContentType'] = 'atom';
+
+$service['getProject']['url'] = '/projects/%project_id%.xml';
+$service['getProject']['callType'] = 'GET';
+$service['getProject']['requestContentType'] = 'xml';
+$service['getProject']['responseContentType'] = 'atom';
+
+$service['getIssues']['url'] = '/issues.json';
+$service['getIssues']['callType'] = 'GET';
+$service['getIssues']['requestContentType'] = 'json';
+$service['getIssues']['responseContentType'] = 'json';
+
+$service['getIssue']['url'] = '/issues/%issue_id%.json';
+$service['getIssue']['callType'] = 'GET';
+$service['getIssue']['requestContentType'] = 'json';
+$service['getIssue']['responseContentType'] = 'json';
+
+$service['putIssue']['url'] = '/issues/%issue_id%.json';
+$service['putIssue']['callType'] = 'PUT';
+$service['putIssue']['requestContentType'] = 'json';
+$service['putIssue']['responseContentType'] = 'json';
+$service['putIssue']['data'] = isset($_GET['data']) ? $_GET['data'] : null;
+
+if (!isset($_GET['serviceOptions'])) { $_GET['serviceOptions'] = array(); }
+if (!isset($_GET['serviceExtras'])) { $_GET['serviceExtras'] = array(); }
+
+$url_variables = array(
+    'project_id' => (isset($_GET['project_id']) ? $_GET['project_id'] : null),
+    'issue_id' => (isset($_GET['issue_id']) ? $_GET['issue_id'] : null)
+);
+
+if (!isset($_GET['serviceOwner']) || (isset($_GET['serviceOwner']) && $_GET['serviceOwner'] != 'redmine' && $_GET['serviceOwner'] != 'harvest') ) {
+    $_GET['serviceOwner'] = 'redmine';
+}
+
+if (isset($_GET['serviceOwner']) && $_GET['serviceOwner'] == 'redmine') {
+    redmineApiConnect($service[$_GET['serviceName']], $_GET['serviceOptions'], $_GET['serviceExtras']);
+} else {
+    harvestApiConnect(null, $harvestUserId, $harvestPassword);
+}
+
+function harvestApiConnect($service = '', $username = '', $password = '') {
+    global $harvestServerUrl;
+
+    $headers = array(
+        getContentType('json'),
+        'Accept: application/xml',
+        'Authorization: Basic ' . base64_encode($username . ':' . $password)
+    );
+
+    $handle = curl_init();
+
+    $url = $harvestServerUrl;
+
+    curl_setopt($handle, CURLOPT_URL, $url);
+    curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, false);
 	curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
 
-	switch($_GET['method']) {
-		case 'GET':
-		break;
+    switch($service['callType']) {
+        case 'POST':
+            curl_setopt($handle, CURLOPT_POST, true);
+            curl_setopt($handle, CURLOPT_POSTFIELDS, $service['data']);
+            break;
 
-		case 'POST':
-		curl_setopt($handle, CURLOPT_POST, true);
-		curl_setopt($handle, CURLOPT_POSTFIELDS, $_GET['data']);
-		break;
+        case 'PUT':
+            curl_setopt($handle, CURLOPT_CUSTOMREQUEST, 'PUT');
+            curl_setopt($handle, CURLOPT_POSTFIELDS, $service['data']);
+            break;
 
-		case 'PUT':
-		curl_setopt($handle, CURLOPT_CUSTOMREQUEST, 'PUT');
-		curl_setopt($handle, CURLOPT_POSTFIELDS, $_GET['data']);
-		break;
+        case 'DELETE':
+            curl_setopt($handle, CURLOPT_CUSTOMREQUEST, 'DELETE');
+            break;
+    }
 
-		case 'DELETE':
-		curl_setopt($handle, CURLOPT_CUSTOMREQUEST, 'DELETE');
-		break;
-	}
+    $response = curl_exec($handle);
+    $code = curl_getinfo($handle, CURLINFO_HTTP_CODE);
 
-	$response = curl_exec($handle);
-	$code = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+    header(getContentType('xml'));
+    echo $response;
+}
 
-	echo $response;
-	
-	function getContentType($type) {
-		switch ($type) {
-			case "atom":
-				return 'Content-type: application/atom+xml';
-			break;
-			
-			case "json":
-				return 'Content-type: application/json';
-			break;
-			
-			case "css":
-				return 'Content-type: text/css';
-			break;
-			
-			case "javascript":
-				return 'Content-type: text/javascript';
-			break;
-			
-			case "rss":
-				return 'Content-Type: application/rss+xml; charset=ISO-8859-1';
-			break;
-			
-			case "xml":
-				return 'Content-type: text/xml';
-			break;
-			
-			default:
-				return 'Content-type: text'
-			break;
-		}
-	}
+function redmineApiConnect(array $service, array $options = array(), array $url_variables = array()) {
+    global $redmineServerUrl;
+    global $redmineApiKey;
+
+    foreach($url_variables as $key => $replacement) {
+        $service['url'] = str_replace('%' . $key . '%', $replacement, $service['url']);
+    }
+
+    $headers = array(getContentType($service['requestContentType']));
+    $handle = curl_init();
+
+    $url = $redmineServerUrl . $service['url'] . '?' . http_build_query($options + array('key' => $redmineApiKey));
+
+    curl_setopt($handle, CURLOPT_URL, $url);
+    curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
+
+    switch($service['callType']) {
+        case 'POST':
+            curl_setopt($handle, CURLOPT_POST, true);
+            curl_setopt($handle, CURLOPT_POSTFIELDS, $service['data']);
+            break;
+
+        case 'PUT':
+            curl_setopt($handle, CURLOPT_CUSTOMREQUEST, 'PUT');
+            curl_setopt($handle, CURLOPT_POSTFIELDS, $service['data']);
+            break;
+
+        case 'DELETE':
+            curl_setopt($handle, CURLOPT_CUSTOMREQUEST, 'DELETE');
+            break;
+    }
+
+    $response = curl_exec($handle);
+    $code = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+
+    header(getContentType($service['responseContentType']));
+    echo $response;
+}
+
+function getContentType($type) {
+    switch ($type) {
+        case 'atom':
+            return 'Content-type: application/atom+xml';
+            break;
+
+        case 'json':
+            return 'Content-type: application/json';
+            break;
+
+        case 'css':
+            return 'Content-type: text/css';
+            break;
+
+        case 'javascript':
+            return 'Content-type: text/javascript';
+            break;
+
+        case 'rss':
+            return 'Content-Type: application/rss+xml; charset=ISO-8859-1';
+            break;
+
+        case 'xml':
+            return 'Content-type: text/xml';
+            break;
+    }
+}
 ?>
